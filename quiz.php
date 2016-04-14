effd3504e1f691afd52c94db4d87188dcf46b844
dasilvad <silva10.daniel@gmail.com>
2016-04-13 23:21:00 -0300

    criado formulario cadastro perguntas

---
+++ quiz.php
@@ -0,0 +1,22 @@
+<?php echo "ha";?>
+<html>
+<head><title></title></head>
+
+<body>
+
+<div id="perguntas">
+Pergunta x<br><br>
+
+<form method="POST" action="">
+  Qual é a pergunta? <input type="text" name="pergunta"><br>
+  Resposta correta: <input type="text" name="resposta_correta"><br>
+  Resposta Errada 1: <input type="text" name="resposta_incorreta1"><br>
+  Resposta Errada 2: <input type="text" name="resposta_incorreta2"><br>
+  <input type="submit" name="salvar" value="Salvar">
+  <input type="submit" name="proxima_pergunta" value="Próxima Pergunta">
+</form>
+</div>
+
+<body>
+</html>
+
